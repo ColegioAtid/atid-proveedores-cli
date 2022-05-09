0bb43cf6c4056c1d57e5cc25c868b925f37a94a8
Rodrigo Garcia Avila <apple@MacBook-Air-de-Apple.local>
2022-05-09 10:31:32 -0500

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import proveedores from '@/modules/proveedores/store/index'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+ /*  state: {
   },
   getters: {
   },
   mutations: {
   },
   actions: {
-  },
+  }, */
   modules: {
+    proveedores,
   }
 })
