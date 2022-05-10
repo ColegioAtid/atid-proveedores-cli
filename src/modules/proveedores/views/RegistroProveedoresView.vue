af510c723ffd284e60a4e396369ac6c73e632ca8
“OsvaldoGMA” <ogonzalez@atid.edu.mx>
2022-05-10 12:19:07 -0500

    ajuste de vista por components

--- src/modules/proveedores/views/RegistroProveedoresView.vue
+++ src/modules/proveedores/views/RegistroProveedoresView.vue
@@ -1,16 +1,39 @@
 <template>
-    <v-row no-gutters>
-      <v-col cols="12">
-        <proveedores-form-component />
-      </v-col>
-    </v-row>
+  <v-row no-gutters>
+    <v-col cols="12">
+      <v-card class="mx-auto my-6 pa-3" shaped>
+        <h3 class="display-1 text-center">
+          Sistema de registro para proveedores
+        </h3>
+        <p class="my-5 mx-7">
+          Por favor, ingresa los siguientes datos para poder darte de alta en el
+          sistema:
+        </p>
+
+        <datos-proveedor-form />
+        <documentos-proveedor-component />
+
+        <v-card-actions>
+          <v-btn color="success" class="white--text pa-6" @click="sendForm">
+            Enviar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import ProveedoresFormComponent from "../components/ProveedoresFormComponent.vue";
+import DatosProveedorForm from "../components/DatosProveedorForm.vue";
+import DocumentosProveedorComponent from "../components/DocumentosProveedorComponent.vue";
 export default {
-  components: { ProveedoresFormComponent },
+  components: { DatosProveedorForm, DocumentosProveedorComponent },
   name: "ProveedorHomeView",
+  methods: {
+    sendForm() {
+      alert("Hola");
+    },
+  },
 };
 </script>
 
