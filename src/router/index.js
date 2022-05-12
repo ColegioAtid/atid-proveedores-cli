cc9db77c4eac8b861a12a146181b8a2f5919d11d
Rodrigo Garcia Avila <apple@MacBook-Air-de-Apple.local>
2022-05-12 13:09:13 -0500

    router guard

--- src/router/index.js
+++ src/router/index.js
@@ -11,24 +11,24 @@ const routes = [
     path: '/',
     name: 'login',
     component: LoginView,
-    meta:{
-      requiresAuth:false,                  
+    meta: {
+      requiresAuth: false,
     }
   },
   {
     path: '/proveedores',
     ...ProveedoresRouter,
-    meta:{
-      requiresAuth:true,      
-      isAdmin:false,
+    meta: {
+      requiresAuth: true,
+      isAdmin: false,
     }
   },
   {
     path: '/admin',
     ...AdminRouter,
-    meta:{
-      requiresAuth:true,      
-      isAdmin:true,
+    meta: {
+      requiresAuth: true,
+      isAdmin: true,
     }
   },
   {
@@ -48,29 +48,28 @@ const router = new VueRouter({
 
 
 // Protección de rutas
-// Router protection
 router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some ( r => r.meta.requiresAuth );
-  if(requiresAuth){ // ¿La ruta require autenticación?
-    if(isLoggedIn()){ // ¿ El usuario esta logeado ?
+  const requiresAuth = to.matched.some(r => r.meta.requiresAuth);
+  if (requiresAuth) { // ¿La ruta require autenticación?
+    if (isLoggedIn()) { // ¿ El usuario esta logeado ?
       const { rol } = getUserInfo();
-      const isAdminRoute = to.matched.some ( r => r.meta.isAdmin );
-      if(isAdminRoute && rol === 'ADMIN' ){ // ¿Es ruta de admin?       
-        next();            
-      }else if(!isAdminRoute && rol === 'PROVEEDOR'){ // ¿Es ruta de proveedor?       
-        next();         
-      }else{  // es un usuario entrando a una ruta no correspondinete     
-        next("/");        
+      const isAdminRoute = to.matched.some(r => r.meta.isAdmin);
+      if (isAdminRoute && rol === 'ADMIN') { // ¿Es ruta de admin?       
+        next();
+      } else if (!isAdminRoute && rol === 'PROVEEDOR') { // ¿Es ruta de proveedor?       
+        next();
+      } else {  // es un rol entrando a una ruta de otro rol     
+        next("/");
       }
-    } else{ // Redirige al login
-      next("/");   
+    } else { // Redirige al login
+      next("/");
     }
-  }else{    
-    if(isLoggedIn()){ // si el ya está logeado, redirigir a la ruta principal
+  } else {
+    if (isLoggedIn()) { // si el ya está logeado, redirigir a la ruta principal
       const { rol } = getUserInfo();
-      if(rol === 'PROVEEDOR'){ next("/proveedores") }
-      else if(rol === 'ADMIN'){ next("/admin") }
-    }else{ // Redirige al login
+      if (rol === 'PROVEEDOR') { next("/proveedores") }
+      else if (rol === 'ADMIN') { next("/admin") }
+    } else { // Redirige al login
       next();
     }
   }
