de71b84376a8b0c1c367df0fbd2fe8e01822c727
Rodrigo Garcia Avila <apple@MacBook-Air-de-Apple.local>
2022-05-24 08:35:35 -0500

    proyecto finalizado

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath:'/proveedores-atid/',
+  outputDir:'proveedores-atid',  
+  pwa:{
+    iconPaths: {
+      favicon32: "./public/favicon.png",
+      favicon16: "./public/favicon.png",
+      appleTouchIcon: "./public/favicon.png",
+      maskIcon: "./public/favicon.png",
+      msTileImage: "./public/favicon.png"
+    },    
+  },
 })
